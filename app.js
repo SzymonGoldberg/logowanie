ce05e2dacda482f7a15e4c1328c846384be0afd4
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-07-02 12:06:27 +0200

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+//https://www.topcoder.com/thrive/articles/authentication-and-authorization-in-express-js-api-using-jwt
+
+const express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  userRoutes = require("./routes/user");
+
+//Connect to database
+try {
+  mongoose.connect("mongodb://0.0.0.0:27017/usersdb", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    serverSelectionTimeoutMS: 1000
+  });
+  console.log("connected to db");
+} catch (error) {
+  handleError(error);
+}
+process.on('unhandledRejection', error => {
+  console.log('unhandledRejection', error.message);
+});
+
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({
+  extended: true
+}));
+
+//using user route
+app.use(userRoutes);
+
+//setup server to listen on port 8080
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server is live on port 8080");
+})
